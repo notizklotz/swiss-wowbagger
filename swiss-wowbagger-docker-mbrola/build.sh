94d646a3f9504a6162252990ef4c80b5a4e600d1
Adrian Gygax <adrian.gygax@schaltstelle.ch>
2021-09-19 17:05:21 +0200

    Reference base image via hash

---
+++ swiss-wowbagger-docker-mbrola/build.sh
@@ -0,0 +1,2 @@
+docker build -t europe-west6-docker.pkg.dev/swiss-wowbagger/wowbagger-docker-repo/swiss-wowbagger-docker-mbrola . && \
+docker push europe-west6-docker.pkg.dev/swiss-wowbagger/wowbagger-docker-repo/swiss-wowbagger-docker-mbrola
